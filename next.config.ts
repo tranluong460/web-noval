5736e3dda3a886d5c5330664d464d2df66302bd9
Alone <tranluong460@gmail.com>
2025-07-25 01:03:22 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
