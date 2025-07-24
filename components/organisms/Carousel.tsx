118ae8901172455e9451f8327e43cdb230e8cafb
Alone <90520187+tranluong460@users.noreply.github.com>
2025-07-25 00:35:14 +0700

    Fix Carousel client component error

--- components/organisms/Carousel.tsx
+++ components/organisms/Carousel.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { StoryCard } from '@/components/molecules/StoryCard';
